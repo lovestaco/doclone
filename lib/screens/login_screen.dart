44d7befe43d731fffe59a1a9ea88ca71c75b3e88
maneshwar <athreyac4@gmail.com>
2022-10-25 14:45:33 +0530

    connecting auth_repository to googleSignIn, class instance, ConsumerWidget insted of statelessWidget, WidgetRef

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,15 +1,23 @@
 import 'package:doclone/colors.dart';
+import 'package:doclone/repository/auth_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends ConsumerWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authRepositoryProvider).signInWithGoogle();
+  }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(authRepositoryProvider).signInWithGoogle();
+
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
-          onPressed: () {},
+          onPressed: () => signInWithGoogle(ref),
           icon: Image.asset(
             'assets/images/g-logo.png',
             height: 20,
