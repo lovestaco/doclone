cd68e801b5663eb082b77618ab2b5310135f9b4f
maneshwar <athreyac4@gmail.com>
2022-10-28 16:57:09 +0530

    index.js - listener, port setup. packege.json - nodemon, script

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("dfsa");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT | 3001;
+
+const app = express();
+
+app.listen(PORT, "0.0.0.0", function () {
+  console.log(`connected at port ${PORT}`);
+  console.log(" ");
+});
