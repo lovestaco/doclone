b12c8ab2e2ea0f9ffb22f770d0f1b9e725695e32
maneshwar <athreyac4@gmail.com>
2022-10-29 10:56:09 +0530

    Mongo connect

--- server/index.js
+++ server/index.js
@@ -5,6 +5,14 @@ const PORT = process.env.PORT | 3001;
 
 const app = express();
 
+const DB =
+  "mongodb+srv://doclone:doclone@cluster0.xdr8tzp.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("Mongo conn success"))
+  .catch((e) => console.log("Mongo error ", e));
+
 app.listen(PORT, "0.0.0.0", function () {
   console.log(`connected at port ${PORT}`);
   console.log(" ");
