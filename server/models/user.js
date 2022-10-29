a6d4836dceb459c836257f32f2b205dae7ca2034
maneshwar <athreyac4@gmail.com>
2022-10-29 12:01:17 +0530

    user model

---
+++ server/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  profilePic: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
